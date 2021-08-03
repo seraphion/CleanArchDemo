a8ecc0351f6e0df32a757d8e53e771fc7f971e77
seraphion <djurdjevicster@gmail.com>
2021-08-03 12:34:12 +0200

    Course Service Create Functionality, Updated CourseViewModel

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 
 namespace CleanArch.Application.Services
@@ -7,10 +9,19 @@ namespace CleanArch.Application.Services
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
+        }
+
+        public void Create(CourseViewModel course)
+        {
+            var createCourseCommand = new CreateCourseCommand(course.Name, course.Description, course.ImageUrl);
+
+            _bus.SendCommand(createCourseCommand);
         }
 
         public CourseViewModel GetCourses()
--- CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using CleanArch.Domain.Models;
+
 using System.Collections.Generic;
 
 namespace CleanArch.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
         public IEnumerable<Course>? Courses { get; set; }
     }
 }
