5e5e1374199fe7554ae9e73cc91010055ac68308
seraphion <djurdjevicster@gmail.com>
2021-08-03 12:44:24 +0200

    Initial Commit Api Project

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+    }
+}
